2bfb028f9a0c44693bd49be6ade03269de30847b
Marco Crippa <xmarcocrippa@gmail.com>
2022-04-18 00:01:20 -0300

    Adicionando um pouco de css

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Beth Felix'
+    })
